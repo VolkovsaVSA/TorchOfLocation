674e44684fdf8b35abe25220c5bc7a0e29052cf6
Sergei <volkovsa@vsa.su>
2021-02-22 09:27:01 +0500

    Initial Commit

---
+++ TorchOfLocation/TorchOfLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorchOfLocationApp.swift
+//  TorchOfLocation
+//
+//  Created by Sergey Volkov on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TorchOfLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
